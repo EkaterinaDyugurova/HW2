3c6219481c7fd541f8c30e9ba7943319d5437432
Dyugurova Ekaterina <dugurowa.k@yandex.ru>
2022-09-14 19:15:54 +0400

    program

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int price = 1000;
+        int rurbonus = 20;
+        int mili = price/rurbonus;
+        System.out.print(mili);
+    }
+}
+
